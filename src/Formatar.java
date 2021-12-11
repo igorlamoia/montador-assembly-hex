4f1f90f2ff00d8bda6d8c13fe9db79b966977e41
Igor Lamoia Queiroz <igorlamoia@hotmail.com>
2021-12-11 15:59:03 -0300

    Formatar: removing comments and blank lines

--- src/Formatar.java
+++ src/Formatar.java
@@ -3,66 +3,46 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Formatar {
 	public static void formatarArquivo(Path arquivoInicial, Path arquivoFormatado) throws IOException {
+		List<String> codigo = new ArrayList<>(); // conjunto de linhas
+		Map<String, Integer> labels = new HashMap<>(); // label => Posição
 
-        List<String> codigo = new ArrayList<>();
-        Boolean formatar = false;
-        String aux = "";
-        int contador = -1;
-        String label[] = new String[3];
-        int numLabel[] = new int[3];
-        int i = 0;
+		int contador = -1;
+		for (String linha : Files.readAllLines(arquivoInicial, StandardCharsets.UTF_8)) {
+			contador++;
+			linha = linha.split("#")[0].trim(); // tudo que vem antes do comentário
 
-        for (String linha : Files.readAllLines(arquivoInicial, StandardCharsets.UTF_8)) {
-            contador++;
-       
-            if (formatar) {
-            	
-            	if (linha.trim().startsWith("#") || linha.trim().isEmpty()) {
-            		
-            	}else {
-            		formatar = false;
-                	//codigo.add(aux.concat(" ".concat(linha)));
-                	//codigo.add(aux.concat(linha)); OU ESSE PARA CONCATENAR SEM O ESPA�O DEPOIS DO ":"
-            		codigo.add(linha);
-            	}
-            	
-            } else if (linha.trim().startsWith("#") || linha.trim().isEmpty()) {
-                
-            } else if (linha.trim().contains(":")){
-            	//aux = Integer.toString(contador);
-            	aux= linha;
-            	formatar = true;
-            	label[i] = aux.substring(0, aux.indexOf(":"));
-        		numLabel[i] = contador;
-        		i++;
-            	
-   	
-            } else {
-            	codigo.add(linha);
-            }
-        }
-        
-        for (int j=0; j<numLabel.length ; j++) {
-        	
-        	for (String c : codigo) {
-        		
-        		if (c.trim().contains(label[j])) {
-        			//c.replace(label[j], Integer.toString(numLabel[j]));
-        			System.out.println(label[j]);
-        		}
-        		
-        	}
-        }
-        
-        Files.write(arquivoFormatado, codigo);
-        
-        //for (int j=0 ; j<3 ; j++) {
-        	//System.out.println(label[j]+"-->"+numLabel[j]);
-        //}
-    }
+			if(linha.contains(":")) {
+				labels.put(linha.substring(0, linha.indexOf(":")).trim(), contador+1);
+				linha = "";
+				contador++;
+			}
 
+			if(! linha.isBlank())
+				codigo.add(linha);
+			else
+				contador--;
+		}
+
+		// Agora com Branchs bne ou beq
+		contador = -1;
+		for (String linha : codigo) {
+			contador++;
+			if(linha.contains("beq") || linha.contains("bne")) {
+				labels.put("pula: "+linha.split(",")[2] + " ", labels.get(linha.split(",")[2].trim())-contador);
+				linha = "";
+				contador++;
+			}
+		}
+
+		for (String key : labels.keySet())
+			System.out.println("Chave: " + key + "=> Valor: " + labels.get(key));
+
+		Files.write(arquivoFormatado, codigo);
+		}
 }
--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,16 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Main {
-	public static void main(String args[]) throws IOException {
-        //String path = "cod_asm.txt";
-        //LeituraEscrita.escritor(path);
-        //LeituraEscrita.leitor(path);
-    	int qtde_linhas = 0;
+	public static void main(String[] args) throws IOException {
 
-    	Path arquivoInicial = Paths.get("cod_asm.txt");
+        Path arquivoInicial = Paths.get("cod_asm.txt");
         Path arquivoFormatado = Paths.get("cod_asm_formatado.txt");
-        Path arquivoHexadecimal = Paths.get("cod_hexadecimal.txt");
+        // Path arquivoHexadecimal = Paths.get("cod_hexadecimal.txt");
         
         Formatar.formatarArquivo(arquivoInicial, arquivoFormatado);
-        
-        //Montagem.paraHexadecimal(arquivoFormatado, arquivoHexadecimal);
-        
-        
+
+
+        // Montagem.paraHexadecimal(arquivoFormatado, arquivoHexadecimal);
         
         //String hexa = Integer.toString(Integer.parseInt("1111", 2), 16); (Binario --> Hexa)
         //String bin = Integer.toString(Integer.parseInt("ff", 16), 2); (Hexa --> Binario)
