f13e6d92beaebedbd0c41427079fc0c135b3f4de
Igor Lamoia Queiroz <igorlamoia@hotmail.com>
2021-12-12 12:18:02 -0300

    Add: construtor Formatar e separando metodos

--- src/Formatar.java
+++ src/Formatar.java
@@ -4,45 +4,64 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
 public class Formatar {
-	public static void formatarArquivo(Path arquivoInicial, Path arquivoFormatado) throws IOException {
-		List<String> codigo = new ArrayList<>(); // conjunto de linhas
-		Map<String, Integer> labels = new HashMap<>(); // label => Posição
+	private Path arquivoInicial = null;
+	private Path arquivoFormatado = null;
+	private int contador = 0;
+	private List<String> codigo = new ArrayList<>();
+	private Map<String, Integer> labels = new HashMap<>(); // label => Posição
+	private Map<String, Integer> branchs = new HashMap<>(); // numeroLinha => string linha
+	private Map<String, Integer> jumps = new HashMap<>(); // numeroLinha => string linha
 
-		int contador = -1;
-		for (String linha : Files.readAllLines(arquivoInicial, StandardCharsets.UTF_8)) {
-			contador++;
-			linha = linha.split("#")[0].trim(); // tudo que vem antes do comentário
+	public Formatar (Path arquivoInicial, Path arquivoFormatado) throws IOException {
+		this.arquivoInicial = arquivoInicial;
+		this.arquivoFormatado = arquivoFormatado;
+		this.formatarArquivo();
+	}
 
-			if(linha.contains(":")) {
-				labels.put(linha.substring(0, linha.indexOf(":")).trim(), contador+1);
-				linha = "";
-				contador++;
-			}
-
-			if(! linha.isBlank())
-				codigo.add(linha);
-			else
-				contador--;
+	private void formatarArquivo () throws IOException {
+		for (String linha : Files.readAllLines(this.arquivoInicial, StandardCharsets.UTF_8)) {
+			linha = linha.split("#")[0].trim(); // tudo que vem antes do comentário, ignorando o resto
+			
+			if(linha.contains(":"))
+				linha = this.pegaLabelRemoveLinha(linha);
+			
+			this.adicionaLinhaValida(linha);
+			this.verificaBranch(linha);
 		}
 
-		// Agora com Branchs bne ou beq
-		contador = -1;
-		for (String linha : codigo) {
-			contador++;
-			if(linha.contains("beq") || linha.contains("bne")) {
-				labels.put("pula: "+linha.split(",")[2] + " ", labels.get(linha.split(",")[2].trim())-contador);
-				linha = "";
-				contador++;
-			}
+		Iterator<String> keyIterator = labels.keySet().iterator();
+		while(keyIterator.hasNext()){
+			String nextKey = keyIterator.next();
+			System.out.println("Chave: "+ nextKey+" Valor: "+ labels.get(nextKey));
 		}
 
-		for (String key : labels.keySet())
-			System.out.println("Chave: " + key + "=> Valor: " + labels.get(key));
+		Files.write(this.arquivoFormatado, this.codigo);
+	}
 
-		Files.write(arquivoFormatado, codigo);
+	// Se contem labels put(chave: nome da lavel, valor: numero da linha atual)
+	private String pegaLabelRemoveLinha(String linha) {
+		this.labels.put(linha.substring(0, linha.indexOf(":")).trim(), this.contador); 
+		linha = ""; // Limpando a linha pois a label deve sair, é só uma marcação
+		return linha;
+	}
+
+	// O this.contador já pulou a linha, agr sim, verifico se era branch (sempre uma instrução à frente)
+	// Se contem branchs put(chave: nome da label de destino, valor: numero linha atual)
+	private void verificaBranch (String linha) {
+		if(linha.contains("beq") || linha.contains("bne"))
+			this.labels.put("Branch: "+linha.split(",")[2].trim(), this.contador);
+	}
+
+	// Somente se a linha for preenchida com código iremos adicionar 
+	private void adicionaLinhaValida (String linha) {
+		if(! linha.isBlank()) {
+			this.codigo.add(linha);
+			this.contador++;
 		}
+	}
 }
--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,9 @@ import java.nio.file.Paths;
 public class Main {
 	public static void main(String[] args) throws IOException {
 
-        Path arquivoInicial = Paths.get("cod_asm.txt");
-        Path arquivoFormatado = Paths.get("cod_asm_formatado.txt");
+        new Formatar(Paths.get("cod_asm.txt"), Paths.get("cod_asm_formatado.txt")); // (arquivo_inicial, arquivo_formatado)
+
         // Path arquivoHexadecimal = Paths.get("cod_hexadecimal.txt");
-        
-        Formatar.formatarArquivo(arquivoInicial, arquivoFormatado);
-
-
         // Montagem.paraHexadecimal(arquivoFormatado, arquivoHexadecimal);
         
         //String hexa = Integer.toString(Integer.parseInt("1111", 2), 16); (Binario --> Hexa)
