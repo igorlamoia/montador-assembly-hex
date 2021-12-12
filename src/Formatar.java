f6fe92e62ce71d75d1e52f54f5b74ceaf05fb5e6
Igor Lamoia Queiroz <igorlamoia@hotmail.com>
2021-12-12 14:10:28 -0300

    Ataulizando branchs descolando

--- src/Formatar.java
+++ src/Formatar.java
@@ -13,9 +13,10 @@ public class Formatar {
 	private Path arquivoFormatado = null;
 	private int contador = 0;
 	private List<String> codigo = new ArrayList<>();
+	private List<String> codigoFormatado = new ArrayList<>();
 	private Map<String, Integer> labels = new HashMap<>(); // label => Posição
-	private Map<String, Integer> branchs = new HashMap<>(); // numeroLinha => string linha
-	private Map<String, Integer> jumps = new HashMap<>(); // numeroLinha => string linha
+	private Map<Integer, String> branchs = new HashMap<>(); // numeroLinha => string linha
+	private Map<Integer, String> jumps = new HashMap<>(); // numeroLinha => string linha
 
 	public Formatar (Path arquivoInicial, Path arquivoFormatado) throws IOException {
 		this.arquivoInicial = arquivoInicial;
@@ -30,8 +31,9 @@ public class Formatar {
 			if(linha.contains(":"))
 				linha = this.pegaLabelRemoveLinha(linha);
 			
-			this.adicionaLinhaValida(linha);
-			this.verificaBranch(linha);
+			this.verificarJump(linha);
+			this.verificarBranch(linha);
+			this.adicionarLinhaValida(linha);
 		}
 
 		Iterator<String> keyIterator = labels.keySet().iterator();
@@ -39,8 +41,19 @@ public class Formatar {
 			String nextKey = keyIterator.next();
 			System.out.println("Chave: "+ nextKey+" Valor: "+ labels.get(nextKey));
 		}
+		Iterator<Integer> chave = branchs.keySet().iterator();
+		while(chave.hasNext()){
+			Integer nextKey = chave.next();
+			System.out.println("Chaves: "+ nextKey+" Valor: "+ branchs.get(nextKey));
+		}
+		Iterator<Integer> chaves = jumps.keySet().iterator();
+		while(chaves.hasNext()){
+			Integer nextKey = chaves.next();
+			System.out.println("Chavex: "+ nextKey+" Valor: "+ jumps.get(nextKey));
+		}
+		this.trocarBranchsJumpsPorNumero();
 
-		Files.write(this.arquivoFormatado, this.codigo);
+		Files.write(this.arquivoFormatado, this.codigoFormatado);
 	}
 
 	// Se contem labels put(chave: nome da lavel, valor: numero da linha atual)
@@ -50,18 +63,50 @@ public class Formatar {
 		return linha;
 	}
 
-	// O this.contador já pulou a linha, agr sim, verifico se era branch (sempre uma instrução à frente)
-	// Se contem branchs put(chave: nome da label de destino, valor: numero linha atual)
-	private void verificaBranch (String linha) {
-		if(linha.contains("beq") || linha.contains("bne"))
-			this.labels.put("Branch: "+linha.split(",")[2].trim(), this.contador);
-	}
-
 	// Somente se a linha for preenchida com código iremos adicionar 
-	private void adicionaLinhaValida (String linha) {
+	private void adicionarLinhaValida (String linha) {
 		if(! linha.isBlank()) {
 			this.codigo.add(linha);
 			this.contador++;
 		}
 	}
+
+	// O this.contador já pulou a linha, agr sim, verifico se era branch (sempre uma instrução à frente)
+	// Se contem branchs put(chave: nome da label de destino, valor: numero linha atual)
+	private void verificarBranch (String linha) {
+		// this.branchs.put(this.contador, linha.trim());
+		if(linha.contains("beq") || linha.contains("bne"))
+			this.branchs.put(this.contador, linha.split(",")[2].trim());
+	}
+
+	// ! Adicionar trim para pegar laber msm se o jump tiver cheio de espaço
+	private void verificarJump (String linha) {
+		// this.jumps.put(this.contador, linha.trim());
+		if(linha.contains("j "))
+			this.jumps.put(this.contador, linha.split(" ")[1].trim());
+	}
+
+	private void trocarBranchsJumpsPorNumero () {
+		String label = null;
+		String metadeEsquerdaDaLabelBranch = null;
+		this.contador = 0;
+		int deslocamento = 0;
+		for(String linha : this.codigo) {
+			if(this.branchs.containsKey(this.contador)){
+				label = this.branchs.get(this.contador).trim(); 
+				metadeEsquerdaDaLabelBranch = linha.split(label)[0].trim();
+				deslocamento = (this.labels.get(label.trim())) - this.contador; 
+				linha = metadeEsquerdaDaLabelBranch + deslocamento;
+			}
+			else if (this.jumps.containsKey(this.contador)){
+				linha = linha.split(" ")[0].trim();
+				linha = linha + " " + this.labels.get(this.jumps.get(this.contador));
+			}
+			this.codigoFormatado.add(linha);
+			this.contador++;
+		}
+	}
+
+
+
 }
